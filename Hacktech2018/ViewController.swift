3d63d4ec2c7ffc7691305b21c6816339f1e97da5
Stanley Kim <stanlejk@usc.edu>
2018-03-02 23:58:34 -0800

    Created Swift project

---
+++ Hacktech2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hacktech2018
+//
+//  Created by Stanley Kim on 3/2/18.
+//  Copyright © 2018 Stanley Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
